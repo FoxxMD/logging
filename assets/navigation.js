1dc829aa293a1a75ce122e75930c2a62c0176aef
FoxxMD <FoxxMD@users.noreply.github.com>
2024-02-23 20:39:21 +0000

    Deploying to gh-pages from @ FoxxMD/logging@2d10102c88f5ff83264ed6c22098a440328b4ee9 🚀

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4WSyw6CMBBF/2XWRHyhkZ0m7khcuDQuCo7QOBbSjkRi/HcjPiiKdX1mTu+ddHMBxjNDCIY1iuPiJGmH2oAHheAMQiiFliImNH57opfxkcCDg1Q7CEfDq/dWFRqZqy7FgzhW47s9ytMUdbO/P6mEZa6Mb+G2ZDK2JFQPdHZ4IkeCJHMmsLAjwZwowhLJysBVgcZ/g/bysD+bDgI7RpSnq6J+slFIxaj3IkHjN/hDFEwsiTBdmqaMzR1tIhEjEX5d5VGpTf/1eh1gXX+mpWJd/bqRNdJt3d4Ax0SV7L4CAAA="
